04464ba23235f533d2afc50cf8b3a9e58554c5ac
Mark Mikofski <bwanamarko@yahoo.com>
2012-07-17 00:06:26 -0700

    update testscript to debug pvsystem

--- testscript.py
+++ testscript.py
@@ -7,12 +7,13 @@ Created on Thu Jun 07 09:44:08 2012
 """
 
 #from pvmismatch.pvmodule import PVmodule
-from pvmismatch.pvstring import PVstring
+#from pvmismatch.pvstring import PVstring
+from pvmismatch.pvsystem import PVsystem
 
 
 def test():
-    pvstr = PVstring()
-    return pvstr
+    pvsys = PVsystem()
+    return pvsys
     #pvmod = PVmodule(numberCells=72)
     #pvmod.setSuns(0.5, 0)
     #return pvmod
