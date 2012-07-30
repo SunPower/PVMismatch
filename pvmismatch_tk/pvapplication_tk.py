ad4cd46a5a70442d7cf7d8bba0bb2aff156c9b24
Mark Mikofski <bwanamarko@yahoo.com>
2012-07-29 21:21:52 -0700

    add sunpower logo and buttons to app

--- pvmismatch_tk/pvapplication_tk.py
+++ pvmismatch_tk/pvapplication_tk.py
@@ -5,7 +5,12 @@ Created on Jul 29, 2012
 @author: marko
 """
 from Tkinter import *
+import os
 
+SPLOGO = os.path.join('..', 'res', 'logo.png')
+PVMODULE_TEXT = 'PVmodule'
+PVSTRING_TEXT = 'PVstring'
+PVSYSTEM_TEXT = 'PVsystem'
 ROOT = Tk()
 
 
@@ -19,3 +24,11 @@ class PVapplicaton(Frame):
         Constructor
         """
         Frame.__init__(self, master=None)
+        self.pack()
+
+        self['bg'] = 'black'
+        self.SPlogo = BitmapImage(SPLOGO, cnf={''}, master)
+        self.PVmoduleButton = Button(self, cnf={'text': PVMODULE_TEXT})
+        self.PVstringButton = Button(self, cnf={'text': PVSTRING_TEXT})
+        self.PVsystemButton = Button(self, cnf={'text': PVSYSTEM_TEXT})
+        self.QUIT = Button(self, cnf={'text': 'Quit', 'command': self.quit})
