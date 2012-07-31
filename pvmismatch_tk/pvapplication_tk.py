3c5cf58eec7a680eb8911fa22dbf872de9464a67
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2012-07-31 13:33:02 -0700

    works much better
    * appending .pack to constructor erases location in memory? pointer
    points to none, is nonetype, not frame, or whatever.
    * always put .pack on its own line if you need to use the constructed
    widget, otherwise, it will revert to the next higher widget.

--- pvmismatch_tk/pvapplication_tk.py
+++ pvmismatch_tk/pvapplication_tk.py
@@ -6,7 +6,7 @@ Created on Jul 29, 2012
 """
 from PIL import Image, ImageTk
 from Tkinter import Entry, StringVar, Frame, Label, Scale, Button, Toplevel, \
-    IntVar, OptionMenu, Message, Spinbox, HORIZONTAL, LEFT
+    IntVar, OptionMenu, Message, Spinbox, HORIZONTAL, LEFT, TOP, BOTH
 from pvmodule_tk import PVmodule_tk
 from pvstring_tk import PVstring_tk
 from pvsystem_tk import PVsystem_tk
@@ -32,30 +32,30 @@ class PVapplicaton(Frame):
         Constructor
         """
         Frame.__init__(self, master)
-        master = self.master  # create a shortcut to the top level
         master.title(PVAPP_TXT)  # set title bar
         # if user resizes, expand frame and fill both sides
-        self.pack(side='top', expand=True, fill='both')
+        self.pack(side=TOP, expand=True, fill=BOTH)
 
         # SP logo
+        # set black background, pad sides with 15 points, top/bottom 5 points
         self.config(bg='black', padx='15', pady='5')
-#        self['bg'] = 'black'  # set black background
-#        self['padx'] = '15'  # pad sides with 15 points
-#        self['pady'] = '5'  # pad top/bottom 5 points
         self.SPlogo_png = Image.open(SPLOGO)  # create image object
         # convert image to tk-compatible format
         self.SPlogo = ImageTk.PhotoImage(self.SPlogo_png)
-        Label(self, image=self.SPlogo, borderwidth='0').pack(side=LEFT)
+        self.SPlogoLbl = Label(self, image=self.SPlogo, borderwidth='0')
+        self.SPlogoLbl.pack(side=TOP)
         # Intro text
         introtext = 'PVmismatch calculates I-V and P-V curves as well as the'
         introtext += '  max power point (MPP) for any sized system.'
-        Message(self, text=introtext, bg='black').pack(fill='both')
+        self.introMsg = Message(self, text=introtext, bg='black', fg='white')
+        self.introMsg.pack(side=TOP, fill=BOTH)
 
         # separator
-        Frame(master, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
+        self.separatorLine()
 
         # PVsystem Frame
-        pvsysframe = self.PVsystemFrame = Frame(master).pack(fill='both')
+        pvsysframe = self.PVsystemFrame = Frame(master)
+        pvsysframe.pack(side=TOP, fill=BOTH)
         # number of strings integer variable
         numStr = self.numberStrings = IntVar(self)
         numStr.set(10)  # default
@@ -63,17 +63,19 @@ class PVapplicaton(Frame):
         scaleCnf = {'from_': 1, 'to': MAX_STRINGS, 'orient': HORIZONTAL,
                      'variable': numStr, 'label': 'Number of Strings',
                      'length': '100'}
-        self.PVsystemScale = Scale(pvsysframe, cnf=scaleCnf).pack(side=LEFT)
+        self.PVsystemScale = Scale(pvsysframe, cnf=scaleCnf)
+        self.PVsystemScale.pack(side=LEFT)
         # PVsystem button
         self.PVsystemButton = Button(pvsysframe, cnf={'text': PVSYSTEM_TEXT})
         self.PVsystemButton.pack(side=LEFT)
         self.PVsystemButton['command'] = self.startPVsystem_tk
 
         # separator
-        Frame(master, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
+        self.separatorLine()
 
         # PVstring Frame
-        pvstrframe = self.PVstringFrame = Frame(master).pack(fill='both')
+        pvstrframe = self.PVstringFrame = Frame(master)
+        pvstrframe.pack(side=TOP, fill=BOTH)
         # number of modules integer variable
         numMod = self.numberModules = IntVar(self)
         numMod.set(10)  # default
@@ -81,60 +83,62 @@ class PVapplicaton(Frame):
         scaleCnf = {'from_': 1, 'to': MAX_MODULES, 'orient': HORIZONTAL,
                      'variable': numMod, 'label': 'Number of Modules',
                      'length': '20'}
-        self.PVstringScale = Scale(pvstrframe, cnf=scaleCnf).pack(side=LEFT)
+        self.PVstringScale = Scale(pvstrframe, cnf=scaleCnf)
+        self.PVstringScale.pack(side=LEFT)
         # module ID # integer variable
         modID = self.moduleID = IntVar(self)
         modID.set(1)
         # module ID # spinbox
-        Label(pvstrframe,text='Module ID #').pack(side=LEFT)
+        self.modIDLabel = Label(pvstrframe, text='Module ID #')
+        self.modIDLabel.pack(side=LEFT)
         spinboxCnf = {'from_': 1, 'to': 10,
                       'textvariable': str(modID)}
-        self.PVstringSpinBox = Spinbox(pvstrframe, cnf=spinboxCnf)
-        self.PVstringSpinBox.pack(side=LEFT)
+        self.modIDspinbox = Spinbox(pvstrframe, cnf=spinboxCnf)
+        self.modIDspinbox.pack(side=LEFT)
         # PVmodule button
         self.PVstringButton = Button(pvstrframe, cnf={'text': PVSTRING_TEXT})
         self.PVstringButton.pack(side=LEFT)
         self.PVstringButton['command'] = self.startPVstring_tk
 
         # separator
-        Frame(master, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
+        self.separatorLine()
 
         ## PVmodule
-        pvmodframe = self.PVmoduleFrame = Frame(master).pack(fill='both')
+        pvmodframe = self.PVmoduleFrame = Frame(master)
+        pvmodframe.pack(side=TOP, fill=BOTH)
         self.numberCells = IntVar(self)  # bind numberCells
         self.numberCells.set(MODULE_SIZES[0])  # default value
         self.numberCellsOption = OptionMenu(pvmodframe, self.numberCells,
                                             *MODULE_SIZES)
-        self.numberCellsOption.pack({'side': LEFT})
-        self.moduleIDlabel = Label(pvmodframe,
-                                   cnf={'text': 'Module ID'})
-        self.moduleIDlabel.pack(side=LEFT)
-        self.moduleID = StringVar(self)  # bind moduleID
-        self.moduleID.set(1)
-        self.moduleIDentry = Entry(pvmodframe,
-                                   textvariable=self.moduleID)
-        self.moduleIDentry.insert(0, '1')
-        self.moduleIDentry.pack(side=LEFT)
+        self.numberCellsOption.pack(side=LEFT)
+        self.cellIDlabel = Label(pvmodframe, text='Cell ID #')
+        self.cellIDlabel.pack(side=LEFT)
+        cellID = self.cellID = StringVar(self)  # bind moduleID
+        self.cellID.set(1)
+        spinboxCnf = {'from_': 1, 'to': 72,
+                      'textvariable': str(cellID)}
+        self.cellIDspinbox = Spinbox(pvmodframe, cnf=spinboxCnf)
+        self.cellIDspinbox.pack(side=LEFT)
         self.PVmoduleButton = Button(pvmodframe,
                                      cnf={'text': PVMODULE_TEXT})
-        self.PVmoduleButton.pack({'side': LEFT})
+        self.PVmoduleButton.pack(side=LEFT)
         self.PVmoduleButton['command'] = self.startPVmodule_tk
-        Frame(self, cnf={'height': '2', 'bg': 'white'}).pack({'fill': 'both'})
 
         # separator
-        Frame(master, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
+        self.separatorLine()
 
         # toolbar
-        toolbar = self.toolbarframe = Frame(master).pack(fill='both')
+        toolbar = self.toolbarframe = Frame(master)
+        toolbar.pack(side=TOP, fill=BOTH)
         self.RESET = Button(toolbar, cnf={'text': 'Reset',
                                           'command': self.reset})
-        self.RESET.pack({'side': 'left', 'fill': 'both'})
+        self.RESET.pack({'side': 'left', 'fill': BOTH})
         self.LOAD = Button(toolbar, cnf={'text': 'Load', 'command': self.load})
-        self.LOAD.pack({'side': 'left', 'fill': 'both'})
+        self.LOAD.pack({'side': 'left', 'fill': BOTH})
         self.SAVE = Button(toolbar, cnf={'text': 'Save', 'command': self.save})
-        self.SAVE.pack({'side': 'left', 'fill': 'both'})
+        self.SAVE.pack({'side': 'left', 'fill': BOTH})
         self.QUIT = Button(toolbar, cnf={'text': 'Quit', 'command': self.quit})
-        self.QUIT.pack({'side': 'left', 'fill': 'both'})
+        self.QUIT.pack({'side': 'left', 'fill': BOTH})
 
     def reset(self):
         print 'reset'
@@ -165,3 +169,7 @@ class PVapplicaton(Frame):
         app.mainloop()
         # please destroy me or I'll continue to run in background
         top.destroy()
+
+    def separatorLine(self):
+        sepCnf = {'height': '2', 'bg': 'white'}
+        Frame(self.master, cnf=sepCnf).pack(side=TOP, fill=BOTH)
