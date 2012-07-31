51fcb8a831408b2013e1f409be730a344f82283c
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2012-07-31 14:35:27 -0700

    fix logo/message, don't allow resizing, add names
    * names can only be added when instantiated, must start with lower case
    * default name of first frame is 'pvapp'
    * name spLogoLabel and introMsg
    * either fill=BOTH or expand=True and anchor=W made logo show up on left
    side, just anchor=W didn't do it
    * need width or aspect (default is 150%) to flatten message to one line
    * use master.resizable(False, False) to restrict resizing in both
    dimensions
    *

--- pvmismatch_tk/pvapplication_tk.py
+++ pvmismatch_tk/pvapplication_tk.py
@@ -5,13 +5,13 @@ Created on Jul 29, 2012
 @author: marko
 """
 from PIL import Image, ImageTk
-from Tkinter import Entry, StringVar, Frame, Label, Scale, Button, Toplevel, \
-    IntVar, OptionMenu, Message, Spinbox, HORIZONTAL, LEFT, TOP, BOTH
+from Tkinter import StringVar, Frame, Label, Scale, Button, Toplevel, IntVar, \
+    OptionMenu, Message, Spinbox, HORIZONTAL, LEFT, TOP, BOTH, W
 from pvmodule_tk import PVmodule_tk
 from pvstring_tk import PVstring_tk
 from pvsystem_tk import PVsystem_tk
 import os
-MODULE_SIZES = [72, 96, 128]
+MOD_SIZES = [72, 96, 128]
 MAX_STRINGS = 100
 MAX_MODULES = 20
 
@@ -31,29 +31,32 @@ class PVapplicaton(Frame):
         """
         Constructor
         """
-        Frame.__init__(self, master)
-        master.title(PVAPP_TXT)  # set title bar
+        Frame.__init__(self, master, name='pvapp')
+        master.resizable(False, False)
+        master.title(PVAPP_TXT)  # set title bar of root (a.k.a. master)
+        # set black background, pad sides with 15 points, top/bottom 5 points
+        self.config(bg='black', padx='15', pady='5')
         # if user resizes, expand frame and fill both sides
         self.pack(side=TOP, expand=True, fill=BOTH)
 
         # SP logo
-        # set black background, pad sides with 15 points, top/bottom 5 points
-        self.config(bg='black', padx='15', pady='5')
         self.SPlogo_png = Image.open(SPLOGO)  # create image object
-        # convert image to tk-compatible format
+        # convert image to tk-compatible format (.gif, .pgm, or .ppm)
         self.SPlogo = ImageTk.PhotoImage(self.SPlogo_png)
-        self.SPlogoLbl = Label(self, image=self.SPlogo, borderwidth='0')
-        self.SPlogoLbl.pack(side=TOP)
+        self.SPlogoLabel = Label(self, image=self.SPlogo, borderwidth='0',
+                               anchor=W, name='spLogoLabel', bg='black')
+        self.SPlogoLabel.pack(side=TOP, expand=True, fill=BOTH)
         # Intro text
         introtext = 'PVmismatch calculates I-V and P-V curves as well as the'
         introtext += '  max power point (MPP) for any sized system.'
-        self.introMsg = Message(self, text=introtext, bg='black', fg='white')
-        self.introMsg.pack(side=TOP, fill=BOTH)
+        self.introMsg = Message(self, text=introtext, bg='black', fg='white',
+                                anchor=W, name='introMsg', width='800')
+        self.introMsg.pack(side=TOP, expand=True, fill=BOTH)
 
         # separator
         self.separatorLine()
 
-        # PVsystem Frame
+        # PVsystem frame
         pvsysframe = self.PVsystemFrame = Frame(master)
         pvsysframe.pack(side=TOP, fill=BOTH)
         # number of strings integer variable
@@ -62,7 +65,7 @@ class PVapplicaton(Frame):
         # number of strings scale (slider)
         scaleCnf = {'from_': 1, 'to': MAX_STRINGS, 'orient': HORIZONTAL,
                      'variable': numStr, 'label': 'Number of Strings',
-                     'length': '100'}
+                     'length': '500'}
         self.PVsystemScale = Scale(pvsysframe, cnf=scaleCnf)
         self.PVsystemScale.pack(side=LEFT)
         # PVsystem button
@@ -73,7 +76,7 @@ class PVapplicaton(Frame):
         # separator
         self.separatorLine()
 
-        # PVstring Frame
+        # PVstring frame
         pvstrframe = self.PVstringFrame = Frame(master)
         pvstrframe.pack(side=TOP, fill=BOTH)
         # number of modules integer variable
@@ -82,15 +85,16 @@ class PVapplicaton(Frame):
         # number of strings scale (slider)
         scaleCnf = {'from_': 1, 'to': MAX_MODULES, 'orient': HORIZONTAL,
                      'variable': numMod, 'label': 'Number of Modules',
-                     'length': '20'}
+                     'length': '100'}
         self.PVstringScale = Scale(pvstrframe, cnf=scaleCnf)
         self.PVstringScale.pack(side=LEFT)
         # module ID # integer variable
         modID = self.moduleID = IntVar(self)
         modID.set(1)
-        # module ID # spinbox
+        # module ID # label
         self.modIDLabel = Label(pvstrframe, text='Module ID #')
         self.modIDLabel.pack(side=LEFT)
+        # module ID # spinbox
         spinboxCnf = {'from_': 1, 'to': 10,
                       'textvariable': str(modID)}
         self.modIDspinbox = Spinbox(pvstrframe, cnf=spinboxCnf)
@@ -103,16 +107,19 @@ class PVapplicaton(Frame):
         # separator
         self.separatorLine()
 
-        ## PVmodule
+        ## PVmodule frame
         pvmodframe = self.PVmoduleFrame = Frame(master)
         pvmodframe.pack(side=TOP, fill=BOTH)
-        self.numberCells = IntVar(self)  # bind numberCells
-        self.numberCells.set(MODULE_SIZES[0])  # default value
-        self.numberCellsOption = OptionMenu(pvmodframe, self.numberCells,
-                                            *MODULE_SIZES)
+        # number of cells integer variable
+        numCells = self.numberCells = IntVar(self)  # bind numberCells
+        numCells.set(MOD_SIZES[0])  # default value
+        # number of cells option menu
+        self.numberCellsOption = OptionMenu(pvmodframe, numCells, *MOD_SIZES)
         self.numberCellsOption.pack(side=LEFT)
+        # cell ID # label
         self.cellIDlabel = Label(pvmodframe, text='Cell ID #')
         self.cellIDlabel.pack(side=LEFT)
+        # cell ID # spinbox
         cellID = self.cellID = StringVar(self)  # bind moduleID
         self.cellID.set(1)
         spinboxCnf = {'from_': 1, 'to': 72,
