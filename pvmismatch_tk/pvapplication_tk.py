27bb2c01528f0828789d21f48870985b905811a8
Mark Mikofski <bwanamarko@yahoo.com>
2012-07-30 23:31:16 -0700

    refactor pvapplication
    * divide into sections use frames and thin which frames as separators
    * TODO try grid?
    * put optionmenu for numbercells on pvapplicaton
    * TODO use spinbox instead of optionmenu?
    * reorder: logo, pvsystem, pvstring, pvmodule
    * scale widget to select # of strings in system and # of modules per
    string
    * TODO orient scale widgets horizontally
    * TODO use spinbox instead? advantage is can enter value, smaller, uses
    pretty up/down arrows

--- pvmismatch_tk/pvapplication_tk.py
+++ pvmismatch_tk/pvapplication_tk.py
@@ -5,11 +5,13 @@ Created on Jul 29, 2012
 @author: marko
 """
 from PIL import Image, ImageTk
-from Tkinter import Frame, Label, Button, Toplevel
+from Tkinter import Frame, Label, Scale, Button, Toplevel, IntVar, OptionMenu
+from Tkinter import Entry, StringVar
 from pvmodule_tk import PVmodule_tk
 from pvstring_tk import PVstring_tk
 from pvsystem_tk import PVsystem_tk
 import os
+MODULE_SIZES = [72, 96, 128]
 
 
 SPLOGO = os.path.join('res', 'logo_bg.png')
@@ -29,8 +31,9 @@ class PVapplicaton(Frame):
         """
         Frame.__init__(self, master)
         self.pack(expand=True)  # if user resizes, expand Frame
-        self.pack(fill='both')
+        self.pack(fill='both')  # if user resizes, fill both sides
 
+        # SP logo
         self['bg'] = 'black'  # set black background
         self['padx'] = '15'  # pad sides with 15 points
         self['pady'] = '5'  # pad top/bottom 5 points
@@ -40,19 +43,46 @@ class PVapplicaton(Frame):
         self.SPlogo = ImageTk.PhotoImage(self.image)
         self.SPlogoLabel = Label(self, image=self.SPlogo,
                                  cnf={'borderwidth': '0'})
-        self.SPlogoLabel.pack({'side': 'top'})
+        self.SPlogoLabel.pack()  # side='top' is default
+        # separator
+        Frame(self, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
 
-        self.PVmoduleButton = Button(self, cnf={'text': PVMODULE_TEXT})
-        self.PVmoduleButton.pack({'side': 'top', 'fill': 'both'})
-        self.PVmoduleButton['command'] = self.startPVmodule_tk
+        # PVsystem Frame
+        self.PVsystemFrame = Frame(self).pack(fill='both')
+        self.PVsystemScale = Scale(self.PVsystemFrame)
+        self.PVsystemScale.pack(side='left')
+        self.PVsystemButton = Button(self.PVsystemFrame, cnf={'text': PVSYSTEM_TEXT})
+        self.PVsystemButton.pack({'side': 'left'})
+        self.PVsystemButton['command'] = self.startPVsystem_tk
+        Frame(self, cnf={'height': '2', 'bg': 'white'}).pack(fill='both')
 
-        self.PVstringButton = Button(self, cnf={'text': PVSTRING_TEXT})
+        # PVstring
+        self.PVstringFrame = Frame(self).pack()
+        self.PVstringButton = Button(self.PVstringFrame, cnf={'text': PVSTRING_TEXT})
         self.PVstringButton.pack({'side': 'top', 'fill': 'both'})
         self.PVstringButton['command'] = self.startPVstring_tk
 
-        self.PVsystemButton = Button(self, cnf={'text': PVSYSTEM_TEXT})
-        self.PVsystemButton.pack({'side': 'top', 'fill': 'both'})
-        self.PVsystemButton['command'] = self.startPVsystem_tk
+        ## PVmodule
+        self.PVmoduleFrame = Frame(self).pack(fill='both')
+        self.numberCells = IntVar(self)  # bind numberCells
+        self.numberCells.set(MODULE_SIZES[0])  # default value
+        self.numberCellsOption = OptionMenu(self.PVmoduleFrame, self.numberCells,
+                                            *MODULE_SIZES)
+        self.numberCellsOption.pack({'side': 'left'})
+        self.moduleIDlabel = Label(self.PVmoduleFrame,
+                                   cnf={'text': 'Module ID'})
+        self.moduleIDlabel.pack(side='left')
+        self.moduleID = StringVar(self)  # bind moduleID
+        self.moduleID.set(1)
+        self.moduleIDentry = Entry(self.PVmoduleFrame,
+                                   textvariable=self.moduleID)
+        self.moduleIDentry.insert(0, '1')
+        self.moduleIDentry.pack(side='left')
+        self.PVmoduleButton = Button(self.PVmoduleFrame,
+                                     cnf={'text': PVMODULE_TEXT})
+        self.PVmoduleButton.pack({'side': 'left'})
+        self.PVmoduleButton['command'] = self.startPVmodule_tk
+        Frame(self, cnf={'height': '2', 'bg': 'white'}).pack({'fill': 'both'})
 
         self.QUIT = Button(self, cnf={'text': 'Quit', 'command': self.quit})
         self.QUIT.pack({'side': 'top', 'fill': 'both'})
