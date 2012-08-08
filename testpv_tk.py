65db39ec099396a018ededd900ab2fc652a8892a
Mark Mikofski <bwanamarko@yahoo.com>
2012-08-07 22:58:00 -0700

    use root.geometry() to force size

--- testpv_tk.py
+++ testpv_tk.py
@@ -11,5 +11,7 @@ from Tkinter import Tk
 if __name__ == "__main__":
     root = Tk()
     app = PVapplicaton(root)
+    print root.winfo_geometry()
+    root.geometry("837x655")
     app.mainloop()
     # matplotlib must implement destroy in mainloop
