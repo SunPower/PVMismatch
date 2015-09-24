3c1591893e13e230f1bd6e1d03d25999ed610c1c
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2015-09-24 13:44:07 -0700

    bump version

--- pvmismatch/__init__.py
+++ pvmismatch/__init__.py
@@ -40,6 +40,6 @@ PVstring = pvstring.PVstring
 PVsystem = pvsystem.PVsystem
 
 __author__ = 'mmikofski'
-__version__ = '2.1'
-__release__ = 'Himalayan Ham'
+__version__ = '2.2rc1'
+__release__ = 'Indian Icecream'
 __all__ = ['pvconstants', 'pvcell', 'pvmodule', 'pvstring', 'pvsystem']
