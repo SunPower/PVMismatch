60ab2acdf236d7039b4a1755060e3c9b0465d37d
Mark Mikofski <bwana.marko@yahoo.com>
2018-01-04 15:20:17 -0800

    update travis to test Py3, update setup with correct install and test reqs, add setup.cfg for universal wheel

--- setup.py
+++ setup.py
@@ -14,22 +14,56 @@ try:
 except IOError:
     pass
 
-setup(name=__name__,
-      version=__version__,
-      description='PV Mismatch Calculator',
-      long_description=README,
-      author=__author__,
-      author_email=__email__,
-      url=__url__,
-      license='BSD 3-clause',
-      packages=['pvmismatch', 'pvmismatch.pvmismatch_lib',
-                'pvmismatch.pvmismatch_tk', 'pvmismatch.contrib',
-                'pvmismatch.contrib.gen_coeffs'],
-      requires=['numpy (>=1.8)', 'matplotlib (>=1.3)', 'scipy (>=0.12.0)'],
-      scripts=['pv_tk.py'],
-      package_data={'pvmismatch':
-                        ['pvmismatch_json/messagetext.English.json',
-                         'pvmismatch_json/validationConstants.json',
-                         'res/logo.png', 'res/logo_invert.png',
-                         'res/logo_bg.png', 'docs/conf.py', 'docs/*.rst',
-                         'docs/Makefile', 'docs/make.bat']})
+INSTALL_REQUIRES = [
+    'numpy>=1.13.3', 'matplotlib>=2.1.0', 'scipy>=1.0.0', 'future>=0.16.0',
+    'six>=1.11.0'
+]
+
+TESTS_REQUIRES = [
+    'nose>=1.3.7', 'pytest>=3.2.1', 'sympy>=1.1.1', 'pvlib>=0.5.1'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering',
+]
+
+setup(
+    name=__name__,
+    version=__version__,
+    description='PV Mismatch Calculator',
+    long_description=README,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    license='BSD 3-clause',
+    packages=[
+        'pvmismatch', 'pvmismatch.pvmismatch_lib',
+        'pvmismatch.pvmismatch_tk', 'pvmismatch.contrib',
+        'pvmismatch.contrib.gen_coeffs'
+    ],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRES,
+    scripts=['pv_tk.py'],
+    package_data={
+        'pvmismatch': [
+            'pvmismatch_json/messagetext.English.json',
+            'pvmismatch_json/validationConstants.json',
+            'res/logo.png', 'res/logo_invert.png',
+            'res/logo_bg.png', 'docs/conf.py', 'docs/*.rst',
+            'docs/Makefile', 'docs/make.bat'
+        ]
+    },
+    classifiers=CLASSIFIERS
+)
